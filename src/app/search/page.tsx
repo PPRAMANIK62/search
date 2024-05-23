9dca056e6b54753b9127dd0f505c3edfa071ea9f
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-05-23 10:51:36 +0530

    FEAT: showing the search results and the no results response

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,11 @@
 import { db } from "@/db";
-import { productsTable } from "@/db/schema";
+import { Product, productsTable } from "@/db/schema";
+import { vectorize } from "@/lib/vectorize";
+import { Index } from "@upstash/vector";
 import { sql } from "drizzle-orm";
+import { X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -10,13 +15,17 @@ interface PageProps {
   };
 }
 
+export type CoreProduct = Omit<Product, "createdAt" | "updatedAt">;
+
+const index = new Index<CoreProduct>();
+
 const page = async ({ searchParams }: PageProps) => {
   const query = searchParams.query;
 
   if (Array.isArray(query) || !query) return redirect("/");
 
   // Querying logic (using full text search)
-  let products = await db
+  let products: CoreProduct[] = await db
     .select()
     .from(productsTable)
     .where(
@@ -29,7 +38,78 @@ const page = async ({ searchParams }: PageProps) => {
     )
     .limit(3);
 
-  return <pre>{JSON.stringify(products)}</pre>;
+  if (products.length < 3) {
+    //search products by schemantic similarity
+    const vector = await vectorize(query);
+    const res = await index.query({
+      topK: 5,
+      vector,
+      includeMetadata: true,
+    });
+
+    const vectorProducts = res
+      .filter((existingProduct) => {
+        if (
+          products.some((product) => product.id === existingProduct.id) ||
+          existingProduct.score < 0.9
+        ) {
+          return false;
+        } else {
+          return true;
+        }
+      })
+      .map(({ metadata }) => metadata!);
+
+    products.push(...vectorProducts);
+  }
+
+  if (products.length === 0) {
+    return (
+      <div className=" text-center py-4 bg-white shadow-md rounded-b-md">
+        <X className=" mx-auto h-8 w-8 text-gray-400" />
+        <h3 className=" mt-2 text-sm font-semibold text-gray-900">
+          No Results
+        </h3>
+        <p className=" mt-1 text-sm mx-auto max-w-prose text-gray-500">
+          Sorry, we couldn&apos;t find any matches for{" "}
+          <span className=" text-green-500 font-medium">{query}</span>.
+        </p>
+      </div>
+    );
+  }
+
+  return (
+    <ul className=" py-4 divide-y divide-zinc-100 bg-white shadow-md rounded-b-md">
+      {products.slice(0, 3).map((product) => (
+        <Link href={`/products/${product.id}`} key={product.id}>
+          <li className=" mx-auto py-4 px-8 flex space-x-4">
+            <div className=" relative flex items-center bg-zinc-100 rounded-lg h-40 w-40">
+              <Image
+                loading="eager"
+                fill
+                alt="product-image"
+                src={`/${product.imageId}`}
+              />
+            </div>
+
+            <div className=" w-full flex-1 space-y-2 py-1">
+              <h1 className=" text-lg font-medium text-gray-900">
+                {product.name}
+              </h1>
+
+              <p className=" prose prose-sm text-gray-500 line-clamp-3">
+                {product.description}
+              </p>
+
+              <p className=" text-base font-medium text-gray-900">
+                ${product.price.toFixed(2)}
+              </p>
+            </div>
+          </li>
+        </Link>
+      ))}
+    </ul>
+  );
 };
 
 export default page;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,13 +3,15 @@ import React, { useRef, useState, useTransition } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { Loader2, Search } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const SearchBar = () => {
+  const searchParams = useSearchParams();
+  const defaultQuery = searchParams.get("query") || "";
   const inputRef = useRef<HTMLInputElement>(null);
   const [isSearching, startTransition] = useTransition();
   const router = useRouter();
-  const [query, setQuery] = useState<string>("");
+  const [query, setQuery] = useState<string>(defaultQuery);
 
   const search = () => {
     startTransition(() => {
